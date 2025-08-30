5df68a94703634f72907cfb0c32e5f0d9e190848
Александр <k.sasha.228228@gmail.com>
2025-08-30 14:07:53 +0300

    Fixed linter errors

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const fetchRss = async (url) => {
   const proxyUrl = 'https://allorigins.hexlet.app/get?disableCache=true&url='
-  
+
   return axios.get(`${proxyUrl}${encodeURIComponent(url)}`)
-    .then((response) => response.data.contents)
+    .then(response => response.data.contents)
     .catch(() => {
       const error = new Error('Network error')
       error.isNetworkError = true
